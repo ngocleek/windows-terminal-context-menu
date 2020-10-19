97a6458c81f7038e8fb65392fad8fe1e2721484e
Ryan Le <lkbaongoc@gmail.com>
2020-10-19 10:25:04 -0700

    fix(install, uninstall): Menu item name and icon path

--- install.bat
+++ install.bat
@@ -1,9 +1,12 @@
-reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal/v icon /d %LOCALAPPDATA%\terminal\terminal.ico /f
-reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe\"" /f
-reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal/v icon /d %LOCALAPPDATA%\terminal\terminal.ico /f
-reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe\"" /f
-reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal/v icon /d %LOCALAPPDATA%\terminal\terminal.ico /f
-reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal\command" /d "\"%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe\"" /f
-robocopy .\terminal %LOCALAPPDATA%\terminal /E /IS /IT
-echo "Context Menu For Windows Terminal is Configured Successfully! Try Righ Clicking to see the option."
+reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal" /v icon /d %LOCALAPPDATA%\wt\terminal.ico /f
+reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal\command" /d "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d ." /f
+
+reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal" /v icon /d %LOCALAPPDATA%\wt\terminal.ico /f
+reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal\command" /d "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d ." /f
+
+reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal" /v icon /d %LOCALAPPDATA%\wt\terminal.ico /f
+reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal\command" /d "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d ." /f
+
+xcopy ".\terminal.ico" "%LOCALAPPDATA%\wt\" /S /Q /Y /F
+echo "Windows Terminal context menu setup is successfully!"
 pause
--- uninstall.bat
+++ uninstall.bat
@@ -1,6 +1,6 @@
 reg delete "HKCU\Software\Classes\Directory\shell\Open Windows Terminal" /f
 reg delete "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal" /f
 reg delete "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal" /f
-@RD /S /Q "%LOCALAPPDATA%\terminal"
+@RD /S /Q "%LOCALAPPDATA%\wt"
 echo "Removed Configurations."
 pause
