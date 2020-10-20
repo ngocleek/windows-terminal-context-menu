f8631051e5701477590077831c16945a9c447b2a
Ngoc Leek <lkbaongoc@gmail.com>
2020-10-19 19:07:47 -0700

    fix(install): icon path has white-space

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
-reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal" /v icon /d %LOCALAPPDATA%\wt\terminal.ico /f
+reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal" /v icon /d "%LOCALAPPDATA%\wt\terminal.ico" /f
 reg add "HKCU\Software\Classes\Directory\shell\Open Windows Terminal\command" /d "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d ." /f
 
-reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal" /v icon /d %LOCALAPPDATA%\wt\terminal.ico /f
+reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal" /v icon /d "%LOCALAPPDATA%\wt\terminal.ico" /f
 reg add "HKCU\Software\Classes\Directory\Background\shell\Open Windows Terminal\command" /d "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d ." /f
 
-reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal" /v icon /d %LOCALAPPDATA%\wt\terminal.ico /f
+reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal" /v icon /d "%LOCALAPPDATA%\wt\terminal.ico" /f
 reg add "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Windows Terminal\command" /d "%LOCALAPPDATA%\Microsoft\WindowsApps\wt.exe -d ." /f
 
 xcopy ".\terminal.ico" "%LOCALAPPDATA%\wt\" /S /Q /Y /F
